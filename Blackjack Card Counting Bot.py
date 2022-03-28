fa3840b33e589fa6a4d0bbacd0a4fc08111d42ab
1Makrae <102006065+1Makrae@users.noreply.github.com>
2022-03-27 21:38:20 -0400

    Update Blackjack Card Counting Bot.py

--- Blackjack Card Counting Bot.py
+++ Blackjack Card Counting Bot.py
@@ -2,17 +2,26 @@ def Main():
     cardcounter()
 def cardcounter():
     count = 0
-    deck = decksize()
+    deck =int(decksize())
     cards = []
     while (cards != "stop"):
         cards = cardactivity()
-        card
+        for card in cards:
+            if (card == "A") or (card == "J") or (card == "Q") or (card == "K") or (card == "1"):
+                count = count - 1
+            elif (card == "2") or (card == "3") or (card == "4") or (card == "5") or (card == "6"):
+                count = count + 1
+        print("count:")
+        print(count)
+        print("truecount:")
+        print(count/deck)
+
 
 
 def decksize():
     return input("How many decks are being used total?\n")
 
 def cardactivity():
-    return input("What cards have been played?")
+    return input("What cards have been played?\n")
 
 Main()
